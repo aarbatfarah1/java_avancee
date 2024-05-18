71866d7a27f076969f1656ccb37642e00c98cf97
AARBAT Farah <farah_aarbat@um5.ac.ma>
2024-05-19 00:03:58 +0100

    done3

--- kafkaProject - 2024/src/main/java/com/security/AuthService.java
+++
@@ -1,6 +0,0 @@
-package com.security;
-
-public interface AuthService {
-    boolean authenticate(String username, String password);
-    String generateAuthToken(String username);
-}
--- kafkaProject - 2024/src/main/java/com/security/Authenticator.java
+++
@@ -1,8 +0,0 @@
-package com.security;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Authenticator extends Remote {
-    boolean authenticate(String username, String password) throws RemoteException;
-}
